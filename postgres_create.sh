4d3597bbeb4ffddf24977feeb06f82bd97436435
sriram <sriramjkini@gmail.com>
2024-03-29 22:25:09 +0530

    script

--- postgres_create.sh
+++ postgres_create.sh
@@ -2,4 +2,5 @@ pacman -S docker --noconfirm
 systemctl start docker
 docker pull postgres
 docker run -d --name mydb --network=host -e POSTGRES_PASSWORD=password postgres
+sleep 2
 docker exec -it mydb psql -U postgres
